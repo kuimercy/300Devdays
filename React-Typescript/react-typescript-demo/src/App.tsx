aec313a5f4bf3dc88bdad99dfaa1d1090dcaa618
kuimercy <wanjirumercy2000@gmail.com>
2024-01-26 15:30:34 +0300

    Typescripts basics, string,number,boolean and object

--- React-Typescript/react-typescript-demo/src/App.tsx
+++ React-Typescript/react-typescript-demo/src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import './App.css';
-import { Greet } from './components/Greet';
+import React from "react";
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
+  const personName = {
+    first: "Troy",
+    last: "Smith",
+  };
+
+  const nameList = [
+    {
+      first: "Troy",
+      last: "Smith",
+    },
+    {
+      first: "John",
+      last: "Doe",
+    },
+    {
+      first: "Karl",
+      last: "Walters",
+    },
+  ];
+
   return (
     <div className="App">
-      <Greet name="John" messageCount={20} isLoggedIn={false}/>
+      <Greet name="John" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
