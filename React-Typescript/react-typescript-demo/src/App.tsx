84e8523dbff058573162c0191c881e2d2c7f3f59
kuimercy <wanjirumercy2000@gmail.com>
2024-01-26 06:13:20 +0300

    boolean and number

--- React-Typescript/react-typescript-demo/src/App.tsx
+++ React-Typescript/react-typescript-demo/src/App.tsx
@@ -5,7 +5,7 @@ import { Greet } from './components/Greet';
 function App() {
   return (
     <div className="App">
-      <Greet name="John"/>
+      <Greet name="John" messageCount={20} isLoggedIn={false}/>
     </div>
   );
 }
